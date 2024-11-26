38780b003b3aeb057ad1c0d089feea7a4c1614ec
alir49-RA <alir49@mcmaster.ca>
2024-11-25 23:34:51 -0500

    Moving Player

--- Player.cpp
+++ Player.cpp
@@ -62,12 +62,7 @@ void Player::updatePlayerDir()
                     {
                         myDir = RIGHT;
                     }
-                    break; 
-
-            case 27:  // escape key exits
-                mainGameMechsRef->setExitTrue();  //EXIT FLAG IS NOT BEING hhit
-                break;
-            
+                    break;
             default:
                 break;
         //Imm not sure if we need input = 0 anymore , but do check 
@@ -78,7 +73,7 @@ void Player::updatePlayerDir()
 
 void Player::movePlayer()
 {
-    updatePlayerDir();
+    //updatePlayerDir(); do not need this.
 
     switch (myDir)
     {
@@ -108,7 +103,7 @@ void Player::movePlayer()
 
         case RIGHT:
             playerPos.pos->x++;
-            if (playerPos.pos->x <= mainGameMechsRef->getBoardSizeX() - 1)
+            if (playerPos.pos->x >= mainGameMechsRef->getBoardSizeX() - 1) // it had <= board x - 1 which is why D wasnt working.
                 {
                     playerPos.pos->x = 1;
                 }
@@ -117,39 +112,7 @@ void Player::movePlayer()
         default:
             break;
     }    
-    
-    // if (myDir == RIGHT)
-    // {
-    //     playerPos.pos->x += 1;
-    //     if (playerPos.pos->y > width - 2) 
-    //     {
-    //         playerPos.pos->x = width % playerPos.pos->y; //20 % 19 = 1, so wraps to 1.
-    //     }
-    // }
-    // else if (myDir == LEFT)
-    // {
-    //     playerPos.pos->x -= 1;
-    //     if (playerPos.pos->x < 1)
-    //     {
-    //         playerPos.pos->x = (playerPos.pos->x + width - 2) % width; // (0+20-2) % 20 = 18, so wraps to 18
-    //     }
-    // }
-    // else if (myDir == UP)
-    // {
-    //     playerPos.pos->y -= 1;
-    //     if (playerPos.pos->y < 1)
-    //     {
-    //         playerPos.pos->y = (playerPos.pos->y + length - 2) % length; //(0+10-2) % 10 = 8, so wraps to 8
-    //     }
-    // }
-    // else if(myDir == DOWN)
-    // {
-    //     playerPos.pos->y += 1;
-    //     if (playerPos.pos->y > length - 2)
-    //     {
-    //         playerPos.pos->y = length % playerPos.pos->y; //10 % 9 = 1, so wraps to 1
-    //     }
-    // }
+
 }
 
 // More methods to be added
